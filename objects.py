3e63777fc7918744e192900d6bb86e157d4e527f
Urko Lopez <urkolopezlopez@gmail.com>
2024-06-08 11:17:10 +0200

    Indirect object impl

--- objects.py
+++ objects.py
@@ -1,25 +1,29 @@
-class Integer:
+class PdfObject():
+    def value(self) -> str:
+        pass
+
+class Integer(PdfObject):
     def __init__(self, val: int) -> None:
         self.val = val
     
     def value(self) -> str:
         return f'{self.val}'
 
-class String:
+class String(PdfObject):
     def __init__(self, val: str) -> None:
         self.val = val
     
     def value(self) -> str:
         return f'({self.val})'
     
-class Name:
+class Name(PdfObject):
     def __init__(self, val: str) -> None:
         self.val = val
     
     def value(self) -> str:
         return f'/{self.val}'
     
-class Array:
+class Array(PdfObject):
     def __init__(self, val: list) -> None:
         self.val = val
     
@@ -31,7 +35,7 @@ class Array:
         res = res[:-1]
         return f'[{res}]'
     
-class Dictionary:
+class Dictionary(PdfObject):
     def __init__(self, val: dict) -> None:
         self.val = val
 
@@ -47,6 +51,20 @@ class Dictionary:
         res += '>>'
         return res
 
+class IndirectObject:
+    def __init__(self, number: int, gen: int, obj: PdfObject) -> None:
+        self.number = number
+        self.gen = gen
+        self.obj = obj
+
+    def value(self) -> str:
+        res = f'{self.number} {self.gen} obj\n'
+        res += self.obj.value()
+        res += '\n'
+        res += 'endobj'
+        return res
+
+
 class Stream:
     def __init__(self, d: Dictionary, s: str, number: int, gen: int) -> None:
         self.d = d
@@ -56,13 +74,11 @@ class Stream:
     
     def value(self) -> str:
         res = ''
-        res += f'{self.number} {self.gen} obj'
         res += self.d.value()
         res += '\n'
         res += 'stream'
         res += self.s
         res += 'endstream'
-        res += 'endobj'
         return res
 
 t = Name('Type')
@@ -73,5 +89,6 @@ v = Name('Version')
 vv = Integer(12)
 
 d = Dictionary({t: tv, st: stv, v: vv})
-print(d.value())
+io = IndirectObject(10, 0, d)
+print(io.value())
 
