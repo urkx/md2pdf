10e0df2951af0275e35cdd3cba269f72704b0d1e
Urko López <urkolopezlopez@gmail.com>
2024-07-09 13:00:53 +0200

    doc

--- decode.py
+++ decode.py
@@ -57,7 +57,7 @@ class LZWDecode:
         '''
             LZW Encoding process.
 
-            data: Bytes to encode
+            data: ASCII-encoded bytes to encode
         '''
         idx = len(self.d)
         res = []
