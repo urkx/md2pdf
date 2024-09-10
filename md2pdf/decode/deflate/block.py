be9914f513f6590604b1295a0085e441cf115f97
Urko López <urkolopezlopez@gmail.com>
2024-09-10 10:19:32 +0200

    Import lz77 in deflate block

--- md2pdf/decode/deflate/block.py
+++ md2pdf/decode/deflate/block.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 from enum import Enum
 
+from md2pdf.decode import lz77
+
 class BlockType(Enum):
     UNCOMPRESSED    = 0
-    FIX_COMPRESSION = 1
-    DYN_COMPRESSION = 2
+    FIX_COMPRESSION = 1 # Pre-agreed Huffman tree, defined by Deflate spec
+    DYN_COMPRESSION = 2 # Huffman table supplied
     ERROR           = 3
 
 @dataclass
@@ -18,9 +20,10 @@ class BlockData:
     pass
 
 class Block:
-    def __init__(self, header: BlockHeader, data: BlockData):
+    def __init__(self, header: BlockHeader, raw_data: str):
         self.header = header
-        self.data = data
+        self.raw_data = raw_data
 
-    def process(self):
-        pass
+    def process(self) -> list[lz77.LZ77.Pair | str]:
+        l = lz77.LZ77(self.raw_data, lz77.LZ77.MAX_WIN_SIZE_DEFLATE)
+        return l.code()
